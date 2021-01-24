63941517db615d8fa160bb9a9eb662738969563d
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2021-01-24 17:34:30 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React2
+          Learn React22
         </a>
       </header>
     </div>
